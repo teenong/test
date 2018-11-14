902ee16abb7c855b56940e022809fcc6dc7a0ee4
teenong <45023570+teenong@users.noreply.github.com>
2018-11-14 11:31:20 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'AuMYzoaH/aPF+pExCsxN2v0P8CxtDTOrjjCCJF61lUZZh6QSJVXivq2TyK/Eggfi8wmNj7CQV23Q5fMTOMxU/qAZk+nVLa3At2lTByuq3j8xLCAEU5y3Ri/kq+0S4LWCdsbn68kzamOsxNWFnmo2WQdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'OziPTCtb12JIgj1lgRQKKNQXffZkuASO180ieyF2dNvSx7wNcDupDRsf/va614uf8wmNj7CQV23Q5fMTOMxU/qAZk+nVLa3At2lTByuq3j+fzZTKDJaKV9bqC0f3ZQUO+lDXBExR86xyrLt4V/i2WgdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
